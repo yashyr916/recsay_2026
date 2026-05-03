2b6279adc4d2b70f3e20361c656f19038de4128c
yashyr916 <yashyr916@gmail.com>
2026-05-03 12:13:20 +0530

    upgrade to Next.js 14

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4p5vOvthn2Fq7CrnIRmEXFmEwrrsiX+2ha1CAI0+zJM=\"\n}"
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uLaQ+kHcpOpIgI7TCvNIaSF4F4CBRe6TPz5fluwU68w=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
